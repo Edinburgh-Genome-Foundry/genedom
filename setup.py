065c2f7ed8ce28375a68210b04b500e4650f510e
Zulko <valentin.zulkower@gmail.com>
2018-04-13 17:59:41 +0100

    added sequenticon dependency

--- setup.py
+++ setup.py
@@ -14,4 +14,5 @@ setup(name='genedom',
     keywords="genetic DNA part standardization synthetic biology",
     packages= find_packages(exclude='docs'),
     install_requires=("dnachisel", "snapgene_reader", "pdf_reports",
-                      "pandas", "dna_features_viewer", "python-box"))
+                      "pandas", "dna_features_viewer", "python-box",
+                      "sequenticon"))
