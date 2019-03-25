a9c46515dffd77c67567d4d8cc4e2895d79a24d1
Zulko <valentin.zulkower@gmail.com>
2019-03-25 16:35:03 +0000

    added [reports] tag to dnachisel

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='genedom',
     keywords="genetic DNA part standardization synthetic biology",
     packages= find_packages(exclude='docs'),
     include_package_data=True,
-    install_requires=("dnachisel", "snapgene_reader", "pdf_reports",
+    install_requires=("dnachisel[reports]", "snapgene_reader", "pdf_reports",
                       "pandas", "dna_features_viewer", "python-box",
                       "openpyxl", "flametree", "sequenticon",
                       "dna_features_viewer"))
