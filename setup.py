e8ae4ef0e3414156d8709b9dd36064b245e409aa
Peter Vegh <pvegh@posteo.uk>
2020-09-05 16:35:37 +0100

    License autodetect fix

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('genedom/version.py').read()) # loads __version__
+exec(open("genedom/version.py").read())  # loads __version__
 
-setup(name='genedom',
-      version=__version__,
-      author='Zulko',
-    description='Genetic parts standardization',
-    long_description=open('pypi-readme.rst').read(),
-    license='see LICENSE.txt',
-    url='https://github.com/Edinburgh-Genome-Foundry/genedom',
+setup(
+    name="genedom",
+    version=__version__,
+    author="Zulko",
+    description="Genetic parts standardization",
+    long_description=open("pypi-readme.rst").read(),
+    license="MIT",
+    url="https://github.com/Edinburgh-Genome-Foundry/genedom",
     keywords="genetic DNA part standardization synthetic biology",
-    packages= find_packages(exclude='docs'),
+    packages=find_packages(exclude="docs"),
     include_package_data=True,
-    install_requires=("dnachisel[reports]", "snapgene_reader", "pdf_reports",
-                      "pandas", "dna_features_viewer", "python-box",
-                      "openpyxl", "flametree", "sequenticon",
-                      "dna_features_viewer"))
+    install_requires=(
+        "dnachisel[reports]",
+        "snapgene_reader",
+        "pdf_reports",
+        "pandas",
+        "dna_features_viewer",
+        "python-box",
+        "openpyxl",
+        "flametree",
+        "sequenticon",
+    ),
+)
